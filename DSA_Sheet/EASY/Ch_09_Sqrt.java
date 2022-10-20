2c063df89d625c442b62526f095cc8ea2900b6b3
Sushil Soniwal <soniwalsushil24@gmail.com>
2022-10-20 16:27:43 +0530

    Square Root

--- DSA_Sheet/EASY/Ch_09_Sqrt.java
+++ DSA_Sheet/EASY/Ch_09_Sqrt.java
@@ -1,24 +1,30 @@
+// Todo:- ( Sqrt(x) :- https://leetcode.com/problems/sqrtx/description/ ).
+
 package DSA_Sheet.EASY;
 
 public class Ch_09_Sqrt {
-    
+
+    /** 
+     * ? Method-01 :- Using Binary Search.
+     * ! Time Complexity :- O(logN).
+     * ? Space Complexity :- O(1).
+     */
     public static int mySqrt(int x) {
-        if(x<=1){
+        if (x <= 1) {
             return x;
         }
 
-        int s=1,e=x/2;
-        while(s<=e){
-            int m=s+(e-s)/2;
+        int s = 1, e = x / 2;
+        while (s <= e) {
+            int m = s + (e - s) / 2;
 
-            if(m==x/m){
+            if (m == x / m) {
                 return m;
             }
-            if(m>x/m){
-                e=m-1;
-            }
-            else{
-                s=m+1;
+            if (m > x / m) {
+                e = m - 1;
+            } else {
+                s = m + 1;
             }
         }
 
